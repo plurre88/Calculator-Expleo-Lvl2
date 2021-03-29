5c5cd01ff99a007fdb30070e851c4f8b214acb2f
Johannes Hellman <johanneshellman@hotmail.com>
2021-03-29 06:59:22 +0200

    stringArray fixed
    fixat så att den tömmer ut alla tomma världen i stringArray.

--- Calculator-2/Calculator.cs
+++ Calculator-2/Calculator.cs
@@ -11,23 +11,26 @@ namespace Calculator_2
         {
             var numbers = Array.ConvertAll(problem.Split('+', '-', '*', '/'), int.Parse).ToList();
             var chars = problem.Split('1', '2', '3', '4', '5', '6', '7', '8', '9', '0');
+
+            chars = chars.Where(c => c != "").ToArray();
+
             double sum = numbers[0];
 
             for (int i = 0; i < numbers.Count -1; i++)
             {
-                if (chars[i+1].Equals("+"))
+                if (chars[i].Equals("+"))
                 {
                     sum += numbers[i+1];
                 }
-                else if (chars[i+1].Equals("-"))
+                else if (chars[i].Equals("-"))
                 {
                     sum -= numbers[i + 1];
                 }
-                else if (chars[i+1].Equals("*"))
+                else if (chars[i].Equals("*"))
                 {
                     sum *= numbers[i + 1];
                 }
-                else if (chars[i+1].Equals("/"))
+                else if (chars[i].Equals("/"))
                 {
                     sum /= numbers[i + 1];
                 }
