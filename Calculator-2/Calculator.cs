bf4a99aa58d71db8c2ed4f64e1bcbc17f55ba631
Johannes Hellman <johanneshellman@hotmail.com>
2021-03-29 06:23:09 +0200

    Calc finish

---
+++ Calculator-2/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculator_2
+{
+    public static class Calculator
+    {
+        public static double Calculate(string problem)
+        {
+            var numbers = Array.ConvertAll(problem.Split('+', '-', '*', '/'), int.Parse).ToList();
+            var chars = problem.Split('1', '2', '3', '4', '5', '6', '7', '8', '9', '0');
+            double sum = numbers[0];
+
+            for (int i = 0; i < numbers.Count -1; i++)
+            {
+                if (chars[i+1].Equals("+"))
+                {
+                    sum += numbers[i+1];
+                }
+                else if (chars[i+1].Equals("-"))
+                {
+                    sum -= numbers[i + 1];
+                }
+                else if (chars[i+1].Equals("*"))
+                {
+                    sum *= numbers[i + 1];
+                }
+                else if (chars[i+1].Equals("/"))
+                {
+                    sum /= numbers[i + 1];
+                }
+            }              
+            return sum;
+        }
+    }
+}
---
+++ Calculator-2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Calculator_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Write your math expression and press enter. Remember to stick to the same precedence");
+                Console.WriteLine(Calculator.Calculate(Console.ReadLine()));
+            }
+        }
+    }
+}
